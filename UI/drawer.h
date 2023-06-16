3a8f0b6a74e92169dd550d0de891152729b71e25
idar66913 <idar66913@gmail.com>
2023-06-16 10:52:44 +0300

    added interface and drawer

---
+++ UI/drawer.h
@@ -0,0 +1,120 @@
+﻿//
+// Created by Идар on 27.05.2023.
+//
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+#include <stack>
+#include <vector>
+
+bool drawing = false;
+cv::Point prevPoint;
+cv::Mat canvas(512, 512, CV_8UC3, cv::Scalar(255, 255, 255));
+std::stack<cv::Mat> undoStack;
+std::stack<cv::Mat> redoStack;
+bool ctrlPressed = false;
+cv::Scalar currentColor(0, 0, 0);
+int line_thickness = 3;
+
+class ColorGenerator
+{
+private:
+    std::vector<cv::Scalar> colors;
+    size_t currentIndex;
+
+public:
+    ColorGenerator() : currentIndex(0)
+    {
+        colors = { cv::Scalar(0, 0, 255), cv::Scalar(0, 255, 0), cv::Scalar(255, 0, 0), cv::Scalar(0, 0, 0) };
+    }
+
+    cv::Scalar getNextColor()
+    {
+        const cv::Scalar& color = colors[currentIndex];
+        currentIndex = (currentIndex + 1) % colors.size();
+        return color;
+    }
+};
+
+void onMouse(int event, int x, int y, int, void* userdata)
+{
+    if (event == cv::EVENT_LBUTTONDOWN)
+    {
+        drawing = true;
+        prevPoint = cv::Point(x, y);
+    }
+    else if (event == cv::EVENT_LBUTTONUP)
+    {
+        undoStack.push(canvas.clone());
+        redoStack = std::stack<cv::Mat>();
+        drawing = false;
+    }
+    else if (event == cv::EVENT_MOUSEMOVE)
+    {
+        if (drawing)
+        {
+            cv::Point currentPoint(x, y);
+            cv::line(canvas, prevPoint, currentPoint, currentColor, line_thickness);
+            prevPoint = currentPoint;
+            cv::imshow("Editor", canvas);
+        }
+    }
+}
+
+void undo()
+{
+    if (!undoStack.empty())
+    {
+        redoStack.push(canvas.clone());
+        canvas = undoStack.top().clone();
+        undoStack.pop();
+        cv::imshow("Editor", canvas);
+    }
+}
+
+void redo()
+{
+    if (!redoStack.empty())
+    {
+        undoStack.push(canvas.clone());
+        canvas = redoStack.top().clone();
+        redoStack.pop();
+        cv::imshow("Editor", canvas);
+    }
+}
+
+void changeColor(ColorGenerator& colorGenerator)
+{
+    currentColor = colorGenerator.getNextColor();
+}
+
+void onKeyPressed(int key, ColorGenerator& colorGenerator)
+{
+    if (key == 'c') // Clear canvas
+    {
+        undoStack.push(canvas.clone());
+        redoStack = std::stack<cv::Mat>();
+        canvas = cv::Scalar(255, 255, 255);
+        cv::imshow("Editor", canvas);
+    }
+    else if (key == 'z' && ctrlPressed)
+    {
+        undo();
+    }
+    else if (key == 'y' && ctrlPressed)
+    {
+        redo();
+    }
+    else if (key == 'r')
+    {
+        changeColor(colorGenerator);
+    }
+    else if (key == '+' || key == '=')
+        line_thickness++;
+    else if (key == '-' || key == '_')
+        line_thickness = std::max(1, line_thickness - 1);
+}
+
+void onCtrlPressed(bool pressed)
+{
+    ctrlPressed = pressed;
+}
---
+++ UI/interface.cpp
@@ -0,0 +1,196 @@
+﻿//
+// Created by Иван Грибанов on 11.06.2023.
+//
+
+#include <iostream>
+#include <windows.h>
+#include <string>
+#include <fstream>
+#include <opencv2/opencv.hpp>
+#include <sys/stat.h>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QFileDialog>
+#include <QtCore/QString>
+#include "dirent.h"
+#include "drawer.h"
+#include "header1.h"
+#include <network.h>
+
+std::string ConvertWideCharToUTF8(const wchar_t* wideCharString)
+{
+    int utf8Size = WideCharToMultiByte(CP_UTF8, 0, wideCharString, -1, NULL, 0, NULL, NULL);
+    std::vector<char> utf8Buffer(utf8Size);
+    WideCharToMultiByte(CP_UTF8, 0, wideCharString, -1, utf8Buffer.data(), utf8Size, NULL, NULL);
+    return std::string(utf8Buffer.data());
+}
+
+// Функция загрузки файла изображения
+std::string loadImage(const std::string& filename) {
+    std::ifstream file(filename, std::ios::binary);
+    if (file.is_open()) {
+        // Получаем размер файла
+        file.seekg(0, std::ios::end);
+        std::streampos fileSize = file.tellg();
+        file.seekg(0, std::ios::beg);
+
+        // Создаем буфер для хранения данных файла
+        std::string imageData;
+        imageData.resize(fileSize);
+
+        // Читаем данные файла в буфер
+        file.read(&imageData[0], fileSize);
+
+        // Закрываем файл
+        file.close();
+
+        return imageData;
+    }
+    else {
+        throw std::runtime_error("Failed to open file: " + filename);
+    }
+}
+
+// Функция сохранения файла изображения
+void saveImage(const std::string& filename, const std::string& imageData) {
+    std::ofstream file(filename, std::ios::binary);
+    if (file.is_open()) {
+        // Сохраняем данные изображения в файл
+        file.write(imageData.data(), imageData.size());
+
+        // Закрываем файл
+        file.close();
+    }
+    else {
+        throw std::runtime_error("Failed to save file: " + filename);
+    }
+}
+
+// Функция для конфигурации размера окна
+void configureWindow(int width, int height, const std::string& windowName, int flags = cv::WINDOW_NORMAL) {
+    cv::Mat image = cv::Mat::zeros(height, width, CV_8UC3);
+    cv::rectangle(image, cv::Rect(0, 0, width, height), cv::Scalar(255, 255, 255), cv::FILLED);
+
+    cv::namedWindow(windowName, flags);
+    cv::imshow(windowName, image);
+    cv::waitKey(0);
+    cv::destroyWindow(windowName);
+}
+
+// Функция отрисовки окна (пример)
+void drawWindow(int width, int height) {
+    cv::Mat image = cv::Mat::zeros(height, width, CV_8UC3);
+    cv::putText(image, "Drawing window", cv::Point(10, height / 2),
+        cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(255, 255, 255), 2);
+
+    cv::namedWindow("Window", cv::WINDOW_NORMAL);
+    cv::imshow("Window", image);
+    cv::waitKey(0);
+    cv::destroyAllWindows();
+}
+
+// Функция для вывода сообщения
+void displayMessage(const std::string& message) {
+    cv::Mat image = cv::Mat::zeros(200, 600, CV_8UC3);
+    cv::putText(image, message, cv::Point(10, 100),
+        cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(255, 255, 255), 2);
+
+    cv::namedWindow("Message", cv::WINDOW_NORMAL);
+    cv::imshow("Message", image);
+    cv::waitKey(0);
+    cv::destroyAllWindows();
+}
+
+// Функция для загрузки окна
+void loadWindow(const std::string& windowName) {
+    cv::namedWindow(windowName, cv::WINDOW_NORMAL);
+    cv::waitKey(0);
+    cv::destroyAllWindows();
+}
+
+// Функция для выгрузки окна
+void unloadWindow(const std::string& windowName) {
+    cv::destroyWindow(windowName);
+}
+
+// Функция для конфигурации окна с использованием имени окна
+void configureWindowByName(const std::string& windowName, int width, int height, int flags = cv::WINDOW_NORMAL) {
+    cv::Mat image = cv::Mat::zeros(height, width, CV_8UC3);
+    cv::rectangle(image, cv::Rect(0, 0, width, height), cv::Scalar(255, 255, 255), cv::FILLED);
+
+    cv::namedWindow(windowName, flags);
+    cv::imshow(windowName, image);
+    cv::waitKey(0);
+    cv::destroyWindow(windowName);
+}
+
+// Функция для проверки, является ли файл изображением
+bool isImageFile(const std::string& filename) {
+    std::string extension = filename.substr(filename.find_last_of(".") + 1);
+    return (extension == "jpg" || extension == "jpeg" || extension == "png" || extension == "bmp");
+}
+
+
+// Функция выбора папки в диалоговом окне
+std::string selectFolder(int argc, char *argv[]) {
+    QApplication app(argc, argv);  // Создаем экземпляр QApplication
+
+    QFileDialog dialog;  // Создаем диалоговое окно
+    dialog.setFileMode(QFileDialog::Directory);  // Устанавливаем режим выбора папки
+    dialog.setOption(QFileDialog::ShowDirsOnly);  // Показывать только папки
+    // Показываем диалоговое окно и ждем, пока пользователь выберет папку
+    if (dialog.exec()) {
+        // Получаем выбранную папку
+        QStringList selectedFolders = dialog.selectedFiles();
+        QString folderPath = selectedFolders.at(0);
+
+        // Преобразуем путь в строку типа std::string и возвращаем
+        return folderPath.toStdString();
+    }
+
+    // Если пользователь не выбрал папку, возвращаем пустую строку
+    return "";
+}
+
+cv::Mat resizeCanvas(const cv::Mat& canvas)
+{
+    cv::Mat resizedCanvas;
+    cv::resize(canvas, resizedCanvas, cv::Size(28, 28));
+    return resizedCanvas;
+}
+
+int main(int argc, char* argv[]) {
+    cv::namedWindow("Editor");
+    cv::setMouseCallback("Editor", onMouse);
+
+    ColorGenerator colorGenerator;
+
+    while (true)
+    {
+        cv::imshow("Editor", canvas);
+        int key = cv::waitKey(1);
+
+        if (key == 27)
+            break;
+        else if (key == 224)
+        {
+            onCtrlPressed(true);
+        }
+        else if (key == -224)
+        {
+            onCtrlPressed(false);
+        }
+        else
+        {
+            onKeyPressed(key, colorGenerator);
+        }
+    }
+    canvas = resizeCanvas(canvas);
+    NN recognizer(3, { 500, 20, 10 }, 28 * 28);
+    matrix m;
+    m.setFromCvMat(canvas);
+    recognizer.load("networks.txt");
+    int ans = recognizer.recognize(m);
+    cv::destroyAllWindows();
+    std::cout << "You drew: " << ans << std::endl;
+    return 0;
+}
